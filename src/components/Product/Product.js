b319b19272f922aecf783943787187fd29371e32
Saiful islam <saifulkapasia23@gmail.com>
2021-03-01 21:59:53 +0600

    Pass eventhandler to child component and handle event

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
     const {name, img, seller, price, stock} = props.product;
-    console.log(props.product);
     return (
         <div className="product">
             <div>
@@ -16,6 +17,10 @@ const Product = (props) => {
                 <p>${price}</p>
                 <br/>
                 <p><small>Only {stock} left in stock - order soon</small></p>
+                <button 
+                    onClick={() => props.handleAddProduct(props.product)} 
+                    className='main-button'
+                ><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
             </div>
         </div>
     );
